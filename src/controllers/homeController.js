fab486a07d10572317aead5f9a2231577717afa5
thquan0308 <tranhoangquanthq@gmail.com>
2024-09-14 02:32:07 +0700

    Initial commit

---
+++ src/controllers/homeController.js
@@ -0,0 +1,29 @@
+import db from '../models/index'
+import CRUDService from '../services/CRUDService'
+let getHomePage = async (req, res) => {
+    let data = await db.User.findAll()
+
+    return res.render('homepage.ejs', {
+        data: JSON.stringify(data)
+    })
+}
+
+let getAboudPage = (req, res) => {
+    return res.render('test/about.ejs')
+}
+
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs')
+}
+
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body)
+    console.log(message)
+    return res.send('post crud from server')
+}
+module.exports = {
+    getHomePage: getHomePage,
+    getAboudPage: getAboudPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
+}
---
+++ src/route/web.js
@@ -0,0 +1,17 @@
+import express from "express";
+import homeController from "../controllers/homeController";
+
+let router = express.Router();
+
+let initWebRoutes = (app) => {
+    router.get('/', homeController.getHomePage)
+    router.get('/about', homeController.getAboudPage)
+    router.get('/crud', homeController.getCRUD)
+    router.post('/post-crud', homeController.postCRUD)
+
+    return app.use("/", router)
+}
+
+// module.exports = initWebRoutes;
+
+export default initWebRoutes
