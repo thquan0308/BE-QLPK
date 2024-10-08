50247399a64728aae19e270d0a5446637c6ed22e
thquan0308 <tranhoangquanthq@gmail.com>
2024-10-08 21:55:50 +0700

    import data from Excel and API get allcode

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -65,10 +65,27 @@ let handleEditUser = async (req, res) => {
     let message = await userService.updataUserData(data)
     return res.status(200).json(message)
 }
+
+let getAllCode = async (req, res) => {
+    try {
+        let data = await userService.getAllCodeService(req.query.type)
+        console.log(data)
+        return res.status(200).json(data)
+
+    } catch (e) {
+        console.log('get all code server: ', e)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+}
+
 module.exports = {
     handleLogin: handleLogin,
     handleGetAllUsers: handleGetAllUsers,
     handleCreateNewUser: handleCreateNewUser,
     handleEditUser: handleEditUser,
     handleDeleteUser: handleDeleteUser,
+    getAllCode: getAllCode,
 }
