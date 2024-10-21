e9a09d2b8b64a7d0c7f82b2edfd7809b5f8d7c0c
thquan0308 <tranhoangquanthq@gmail.com>
2024-10-21 23:12:21 +0700

    61 final CRUD BE

--- src/services/userService.js
+++ src/services/userService.js
@@ -173,7 +173,7 @@ let deleteUser = (userId) => {
 let updataUserData = (data) => {
     return new Promise(async (resolve, reject) => {
         try {
-            if (!data.id) {
+            if (!data.id || !data.roleId || !data.positionId || !data.gender) {
                 resolve({
                     errCode: 2,
                     errMessage: 'Missing required parameters'
@@ -187,6 +187,10 @@ let updataUserData = (data) => {
                 user.firstName = data.firstName;
                 user.lastName = data.lastName;
                 user.address = data.address;
+                user.roleId = data.roleId;
+                user.positionId = data.positionId;
+                user.gender = data.gender;
+                user.phonenumber = data.phonenumber;
 
                 await user.save()
 
