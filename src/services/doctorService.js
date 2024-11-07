e159799d9690c97324b6ebd38c4c05e28f0e12b8
thquan0308 <tranhoangquanthq@gmail.com>
2024-11-07 22:08:53 +0700

    79 update and insert infor doctor BE

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -1,3 +1,4 @@
+import { raw } from "body-parser"
 import db from "../models/index"
 import _ from 'lodash'
 require('dotenv').config()
@@ -54,13 +55,24 @@ let getAllDoctors = () => {
 let saveDetailInforDoctor = (inputData) => {
     return new Promise(async (resolve, reject) => {
         try {
-            if (!inputData.doctorId || !inputData.contentHTML
-                || !inputData.contentMarkdown || !inputData.action) {
+            if (!inputData.doctorId
+                || !inputData.contentHTML
+                || !inputData.contentMarkdown
+                || !inputData.action
+                || !inputData.selectedPrice
+                || !inputData.selectedPayment
+                || !inputData.selectedProvince
+                || !inputData.nameClinic
+                || !inputData.addressClinic
+                || !inputData.note
+            ) {
                 resolve({
                     errCode: 1,
                     errMessage: 'Missing parameters'
                 })
             } else {
+
+                //update and insert đến Markdown
                 if (inputData.action === 'CREATE') {
                     await db.Markdown.create({
                         contentHTML: inputData.contentHTML,
@@ -83,6 +95,36 @@ let saveDetailInforDoctor = (inputData) => {
                         await doctorMarkdown.save()
                     }
 
+                }
+
+                //update and insert đến Doctor_infor table
+                let doctorInfor = await db.Doctor_Infor.findOne({
+                    where: { doctorId: inputData.doctorId },
+                    raw: false
+                })
+
+                if (doctorInfor) {
+                    //update
+                    doctorInfor.doctorId = inputData.doctorId
+                    doctorInfor.priceId = inputData.selectedPrice
+                    doctorInfor.provinceId = inputData.selectedProvince
+                    doctorInfor.paymentId = inputData.selectedPayment
+                    doctorInfor.nameClinic = inputData.nameClinic
+                    doctorInfor.addressClinic = inputData.addressClinic
+                    doctorInfor.note = inputData.note
+
+                    await doctorInfor.save()
+                } else {
+                    //create
+                    await db.Doctor_Infor.create({
+                        doctorId: inputData.doctorId,
+                        priceId: inputData.selectedPrice,
+                        provinceId: inputData.selectedProvince,
+                        paymentId: inputData.selectedPayment,
+                        nameClinic: inputData.nameClinic,
+                        addressClinic: inputData.addressClinic,
+                        note: inputData.note,
+                    })
                 }
 
                 resolve({
